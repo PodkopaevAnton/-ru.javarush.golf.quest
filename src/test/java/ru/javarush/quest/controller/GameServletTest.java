42ad29a86ea6b3f0646672f8fe6ca5b20615e296
Anton Podkopaev <test@email.com>
2022-11-21 00:42:36 +0300

    add_gameServlet_test

---
+++ src/test/java/ru/javarush/quest/controller/GameServletTest.java
@@ -0,0 +1,12 @@
+package ru.javarush.quest.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServletTest {
+
+    @Test
+    void doGet() {
+    }
+}
